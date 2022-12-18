8249bc1bb234d4d91717860137e4e218552f41a1
AlineMarra12 <114824105+AlineMarra12@users.noreply.github.com>
2022-12-17 21:58:19 -0300

    Add files via upload

---
+++ gsaGH.js
@@ -0,0 +1,16 @@
+header = document.querySelector("#header")
+
+header.innerHTML = "<h1> Natureza </h1>"
+
+ var main = document.querySelector("#main")
+
+var creat = document.createElement('div')
+
+main. appendChild (creat)
+
+creat.innerHTML= "<h2>  abcdefghijklmnopqrstuvwxyz </h2> <p> aeiou <p>"
+
+var creat2 = document.createElement('div')
+main. appendChild (creat2)
+
+creat2.innerHTML= "<h2>  123456789 </h2> <p> 10111213141516171819 <p>"
